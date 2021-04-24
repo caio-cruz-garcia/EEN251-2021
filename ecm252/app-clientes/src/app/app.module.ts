93cfe652ac8020c6b67fbb41fde015ab11908fc7
caio-cruz-garcia <61151058+caio-cruz-garcia@users.noreply.github.com>
2021-04-24 11:15:13 -0300

    adição dos componentes

---
+++ ecm252/app-clientes/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ClienteInserirComponent } from './clientes/cliente-inserir/cliente-inserir.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { ClienteListaComponent } from './clientes/cliente-lista/cliente-lista.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClienteInserirComponent,
+    CabecalhoComponent,
+    ClienteListaComponent
+  ],
+  imports: [
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
